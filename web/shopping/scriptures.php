744f9f69320979da1c79ea87f13fe5038429ae9c
Taylor Williams <taylordwilliams90@gmail.com>
2019-10-15 21:56:53 -0600

    team work

---
+++ web/shopping/scriptures.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Lets go Shopping</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="pricing.css" rel="stylesheet">
+  </head>
+<body class="container">
+<form action="insert.php" method="post">
+
+    <label for="book">BOOK</label>
+    <input type="text" name="book" id="book">
+
+    <label for="chapter">CHAPTER</label>
+    <input type="text" name="chapter" id="chapter">
+
+    <label for="verse">VERSE</label>
+    <input type="text" name="verse" id="verse">
+
+    <label for="content">CONTENT</label>
+    <input type="text" name="content" id="content">
+
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
